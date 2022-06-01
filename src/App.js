2aff5e686f8328b4bd509d196b302c156d610241
Ryan Raschbaum <ryanraschbaum@Ryans-MacBook-Pro.local>
2022-06-01 18:59:03 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      Hello
+      <div>
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="/">Ryan Raschbaum</a>.
+      </div>
     </div>
   );
 }
