fb8c44e2ca3f6639d45e35ce6054da6cc510a269
Ryan Raschbaum <ryanraschbaum@Ryans-MacBook-Pro.local>
2022-06-01 19:38:07 -0400

    initial constants

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+  violet: "246deg 80% 60%",
+  orange: "15deg 100% 70%",
+  aqua: "195deg 74% 62%",
+  red: "348deg 100% 68%",
+  green: "145deg 58% 55%",
+  purple: "264deg 64% 52%",
+  yellow: "43deg 84% 65%",
+  blue: {
+    100: "236deg 100% 87%",
+    200: "235deg 45% 61%",
+    300: "226deg 43% 10%",
+  },
+};
+
+export const WEIGHTS = {
+  normal: 300,
+  medium: 400,
+  bold: 500,
+};
